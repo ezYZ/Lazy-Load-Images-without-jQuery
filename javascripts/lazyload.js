ef8d5bfb0197031d8cb2d9c722a15c706c3e0596
buffcode <buffcode@users.noreply.github.com>
2014-11-19 08:59:53 +0100

    Code cleanup and optimization
    - Changed indentation to 4 spaces
    - Fixed missing semicolons
    - Fixed common javascript errors as per JSLint / JSHint (function declaration in loops, combined multiple vars, ...)
    - Introduced "use strict"
    - Removed unused function parameters

--- javascripts/lazyload.js
+++ javascripts/lazyload.js
@@ -5,124 +5,121 @@
  * Original by Mike Pulaski - http://www.mikepulaski.com
  * Modified by Kai Zau - http://kaizau.com
  */
-(function() {
-  var addEventListener =  window.addEventListener || function(n,f) { window.attachEvent('on'+n, f); },
-      removeEventListener = window.removeEventListener || function(n,f,b) { window.detachEvent('on'+n, f); };
+(function () {
+    "use strict";
 
-  var lazyLoader = {
-    cache: [],
-    mobileScreenSize: 500,
-    //tinyGif: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
+    var addEventListener = window.addEventListener || function (n, f) { window.attachEvent('on' + n, f); },
+        removeEventListener = window.removeEventListener || function (n, f) { window.detachEvent('on' + n, f); },
+        lazyLoader = {};
 
-    addObservers: function() {
-      addEventListener('scroll', lazyLoader.throttledLoad);
-      addEventListener('resize', lazyLoader.throttledLoad);
-    },
+    lazyLoader = {
+        cache: [],
+        mobileScreenSize: 500,
+        //tinyGif: 'data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==',
 
-    removeObservers: function() {
-      removeEventListener('scroll', lazyLoader.throttledLoad, false);
-      removeEventListener('resize', lazyLoader.throttledLoad, false);
-    },
+        addObservers: function () {
+            addEventListener('scroll', lazyLoader.throttledLoad);
+            addEventListener('resize', lazyLoader.throttledLoad);
+        },
 
-    throttleTimer: new Date().getTime(),
+        removeObservers: function () {
+            removeEventListener('scroll', lazyLoader.throttledLoad, false);
+            removeEventListener('resize', lazyLoader.throttledLoad, false);
+        },
 
-    throttledLoad: function() {
-      var now = new Date().getTime();
-      if ((now - lazyLoader.throttleTimer) >= 200) {
-        lazyLoader.throttleTimer = now;
-        lazyLoader.loadVisibleImages();
-      }
-    },
+        throttleTimer: new Date().getTime(),
 
-    loadVisibleImages: function() {
-      var scrollY = window.pageYOffset || document.documentElement.scrollTop;
-      var pageHeight = window.innerHeight || document.documentElement.clientHeight;
-      var range = {
-        min: scrollY - 200,
-        max: scrollY + pageHeight + 200
-      };
+        throttledLoad: function () {
+            var now = new Date().getTime();
+            if ((now - lazyLoader.throttleTimer) >= 200) {
+                lazyLoader.throttleTimer = now;
+                lazyLoader.loadVisibleImages();
+            }
+        },
 
-      var i = 0;
-      while (i < lazyLoader.cache.length) {
-        var image = lazyLoader.cache[i];
-        var imagePosition = getOffsetTop(image);
-        var imageHeight = image.height || 0;
+        // For IE7 compatibility
+        // Adapted from http://www.quirksmode.org/js/findpos.html
+        getOffsetTop: function (el) {
+            var val = 0;
+            while (el.offsetParent) {
+                val += el.offsetTop;
+                el = el.offsetParent;
+            }
+            return val;
+        },
 
-        if ((imagePosition >= range.min - imageHeight) && (imagePosition <= range.max)) {
-          var mobileSrc = image.getAttribute('data-src-mobile');
+        loadVisibleImages: function () {
+            var scrollY = window.pageYOffset || document.documentElement.scrollTop,
+                pageHeight = window.innerHeight || document.documentElement.clientHeight,
+                range = {
+                    min: scrollY - 200,
+                    max: scrollY + pageHeight + 200
+                },
+                i = 0,
+                onloadFunc = function () {
+                    this.className = this.className.replace(/(^|\s+)lazy-load(\s+|$)/, '$1lazy-loaded$2');
+                };
 
-          image.onload = function() {
-            this.className = this.className.replace(/(^|\s+)lazy-load(\s+|$)/, '$1lazy-loaded$2');
-          };
+            while (i < lazyLoader.cache.length) {
+                var image         = lazyLoader.cache[i],
+                    imagePosition = lazyLoader.getOffsetTop(image),
+                    imageHeight   = image.height || 0;
 
-          if (mobileSrc && screen.width <= lazyLoader.mobileScreenSize) {
-            image.src = mobileSrc;
-          }
-          else {
-            image.src = image.getAttribute('data-src');
-          }
+                if ((imagePosition >= range.min - imageHeight) && (imagePosition <= range.max)) {
+                    var mobileSrc = image.getAttribute('data-src-mobile');
 
-          image.removeAttribute('data-src');
-          image.removeAttribute('data-src-mobile');
+                    image.onload = onloadFunc.bind(image);
 
-          lazyLoader.cache.splice(i, 1);
-          continue;
+                    if (mobileSrc && screen.width <= lazyLoader.mobileScreenSize) {
+                        image.src = mobileSrc;
+                    } else {
+                        image.src = image.getAttribute('data-src');
+                    }
+
+                    image.removeAttribute('data-src');
+                    image.removeAttribute('data-src-mobile');
+
+                    lazyLoader.cache.splice(i, 1);
+                    continue;
+                }
+
+                i++;
+            }
+
+            if (lazyLoader.cache.length === 0) {
+                lazyLoader.removeObservers();
+            }
+        },
+
+        init: function () {
+            // Patch IE7- (querySelectorAll)
+            if (!document.querySelectorAll) {
+                document.querySelectorAll = function (selector) {
+                    var doc = document,
+                        head = doc.documentElement.firstChild,
+                        styleTag = doc.createElement('STYLE');
+                    head.appendChild(styleTag);
+                    doc.__qsaels = [];
+                    styleTag.styleSheet.cssText = selector + "{x:expression(document.__qsaels.push(this))}";
+                    window.scrollBy(0, 0);
+                    return doc.__qsaels;
+                };
+            }
+
+            addEventListener('load', function lazyLoaderInit() {
+                var imageNodes = document.querySelectorAll('img[data-src]');
+
+                for (var i = 0; i < imageNodes.length; i++) {
+                    lazyLoader.cache.push(imageNodes[i]);
+                }
+
+                lazyLoader.addObservers();
+                lazyLoader.loadVisibleImages();
+
+                removeEventListener('load', lazyLoaderInit, false);
+            });
         }
+    };
 
-        i++;
-      }
-
-      if (lazyLoader.cache.length === 0) {
-        lazyLoader.removeObservers();
-      }
-    },
-
-    init: function() {
-      // Patch IE7- (querySelectorAll)
-      if (!document.querySelectorAll) {
-        document.querySelectorAll = function(selector) {
-          var doc = document,
-              head = doc.documentElement.firstChild,
-              styleTag = doc.createElement('STYLE');
-          head.appendChild(styleTag);
-          doc.__qsaels = [];
-          styleTag.styleSheet.cssText = selector + "{x:expression(document.__qsaels.push(this))}";
-          window.scrollBy(0, 0);
-          return doc.__qsaels;
-        }
-      }
-
-      addEventListener('load', function _lazyLoaderInit() {
-        var imageNodes = document.querySelectorAll('img[data-src]');
-
-        for (var i = 0; i < imageNodes.length; i++) {
-          var imageNode = imageNodes[i];
-
-          // Add a placeholder if one doesn't exist
-          //imageNode.src = imageNode.src || lazyLoader.tinyGif;
-
-          lazyLoader.cache.push(imageNode);
-        }
-
-        lazyLoader.addObservers();
-        lazyLoader.loadVisibleImages();
-
-        removeEventListener('load', _lazyLoaderInit, false);
-      });
-    }
-  }
-
-  // For IE7 compatibility
-  // Adapted from http://www.quirksmode.org/js/findpos.html
-  function getOffsetTop(el) {
-    var val = 0;
-    if (el.offsetParent) {
-      do {
-        val += el.offsetTop;
-      } while (el = el.offsetParent);
-      return val;
-    }
-  }
-
-  lazyLoader.init();
-})();
+    lazyLoader.init();
+}());
